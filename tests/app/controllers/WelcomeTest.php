baf0f584db559ba8de807a30cad7ecc50d8c55b5
Stephen <stevewolfe71@gmail.com>
2015-05-03 14:47:57 +0100

    welcome first test

--- tests/app/controllers/WelcomeTest.php
+++ tests/app/controllers/WelcomeTest.php
@@ -2,5 +2,10 @@
 
 class WelcomeTest extends PHPUint_Framework_TestCase{
 
-
+	/**
+	 * @covers controllers\Welcome::__construct
+	 */
+	public function testConstructor(){
+	    $this->assertTrue(true);
+	}
 }
